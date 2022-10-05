e36a2266d97a0602c3e439aaaec15c9551162d46
Maksym <rendon4ik@gmail.com>
2022-10-05 20:57:43 +0300

    Added auth controller with mock data

---
+++ ECampus.Services.Auth/Dtos/UserTokenPayload.cs
@@ -0,0 +1,11 @@
+﻿namespace ECampus.Services.Auth.Dtos
+{
+    public class UserTokenPayload
+    {
+        public string UserId { get; set; } = null!;
+
+        public string Token { get; set; } = null!;
+
+        public string RefreshToken { get; set; } = null!;
+    }
+}
---
+++ ECampus.Services.Auth/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECampus.Services.Auth.Models
+{
+    public class User
+    {
+        [Required]
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
