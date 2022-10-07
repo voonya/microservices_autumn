5caf35e7cc9bea3da40b5c993a659cd75e57f738
Mykyta <nikitochkaaaaaa@gmail.com>
2022-10-07 12:24:07 +0300

    Added profile service front

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,9 @@
 import { useRef } from 'react';
-import FileService from './FileService'
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import FileService from './FileService'
+import AuthService from "./AuthService";
+import ScheduleService from "./ScheduleService";
+import ProfileService from "./ProfileService";
 
 
 function App() {
@@ -11,7 +14,11 @@ function App() {
           <a href="/schedule">schedule</a><br/>
           <a href="/profile">profile</a><br/>
         <Routes>
+
+            <Route path="/auth" element={<AuthService/>}></Route>
             <Route path="/file" element={<FileService/>}></Route>
+            <Route path="/schedule" element={<ScheduleService/>}></Route>
+            <Route path="/profile" element={<ProfileService/>}></Route>
         </Routes>
       </Router>
   );
--- client/src/AuthService.tsx
+++ client/src/AuthService.tsx
@@ -1,7 +1,102 @@
 import {useRef} from 'react';
 
-function AuthService() {
-    return(
-        <div></div>
-)}
-export default AuthService
+function ProfileService() {
+    const responseDiv = useRef<HTMLDivElement>(null);
+    const getProfileIdInput = useRef<HTMLInputElement>(null);
+    const createProfileIdInput = useRef<HTMLInputElement>(null);
+    const updateProfileIdInput = useRef<HTMLInputElement>(null);
+    const deleteProfileIdInput = useRef<HTMLInputElement>(null);
+
+    const makeRequest = (route: string, options?: Record<string, unknown>) => {
+        fetch(route, options)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+                setResponse(JSON.stringify(data, null, 4))
+            }).catch((err) => {
+            console.error(err);
+        });
+    }
+    const setResponse = (response: string) => {
+        if (responseDiv.current) {
+            responseDiv.current.innerHTML = `<pre>${response}</pre>`;
+        }
+    }
+    const getProfileHandler = () => {
+        if (!getProfileIdInput.current?.value) {
+            return;
+        }
+
+        makeRequest(`http://localhost:8080/api/profile/${getProfileIdInput.current.value}`);
+    }
+
+    const createProfileHandler = () => {
+        makeRequest(`http://localhost:8080/api/profile/`, {'method': "POST"});
+    }
+
+    const updateProfileHandler = () => {
+        if (!updateProfileIdInput.current?.value) {
+            return;
+        }
+
+        makeRequest(`http://localhost:8080/api/profile/${updateProfileIdInput.current.value}`);
+    }
+
+    const deleteProfileHandler = () => {
+        if (!deleteProfileIdInput.current?.value) {
+            return;
+        }
+
+        makeRequest(`http://localhost:8080/api/profile/${deleteProfileIdInput.current.value}`);
+    }
+    return (
+        <div className="App">
+            <div className='response-container'>
+                <h2>Response: </h2>
+                <div ref={responseDiv}></div>
+            </div>
+            <section className='service-container'>
+                <h2>Profile service:</h2>
+                <div>
+                    <h3>Get Profile</h3>
+                    <div>
+                        <span>GET</span>
+                        <span>http://localhost:8080/api/profile/:id</span>
+                        <input type='text' ref={getProfileIdInput}></input>
+                        <button onClick={getProfileHandler}>Send</button>
+                    </div>
+                </div>
+                <div>
+                    <h3>Create Profile</h3>
+                    <div>
+                        <span>POST</span>
+                        <span>http://localhost:8080/api/profile/</span>
+                        <input type="profile" ref={createProfileIdInput}/>
+                        <button onClick={createProfileHandler}>Send</button>
+                    </div>
+                </div>
+                <div>
+                    <h3>Update Profile</h3>
+                    <div>
+                        <span>PUT</span>
+                        <span>http://localhost:8080/api/profile/:id</span>
+                        <input type="text" ref={updateProfileIdInput}/>
+                        <button onClick={updateProfileHandler}>Send</button>
+                    </div>
+                </div>
+                <div>
+                    <h3>Delete Profile</h3>
+                    <div>
+                        <span>PUT</span>
+                        <span>http://localhost:8080/api/profile/:id</span>
+                        <input type="text" ref={deleteProfileIdInput}/>
+                        <button onClick={deleteProfileHandler}>Send</button>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
+export default ProfileService
