966ce6cbc27db45d38e5a66fed98c8372e36ddeb
Mykyta <nikitochkaaaaaa@gmail.com>
2022-10-23 12:28:32 +0300

    added db methods

--- Ecampus.Services.Profile/src/db/repositories/profile.ts
+++ Ecampus.Services.Profile/src/db/repositories/profile.ts
@@ -10,7 +10,35 @@ class ProfileRepository {
     }
 
     getById(id: string) {
-        return this._dbClient.findOne({ where: { id }, raw: true });
+        return this._dbClient.findOne({where: { id }, raw: true });
+    }
+
+    createNewUser(data) {
+        return this._dbClient.create(data)
+    }
+
+    deleteById(id: string) {
+        return this._dbClient.destroy({where: { id }})
+    }
+
+    changeName(id: string, first_name: string, last_name: string) {
+        return this._dbClient.update({first_name: first_name, last_name: last_name}, {where: { id }})
+    }
+
+    changeRole(id:string, role_id:string) {
+        return this._dbClient.update({role_id: role_id}, {where: { id }})
+    }
+
+    changeGroup(id: string, group_id: string) {
+        return this._dbClient.update({group_id: group_id}, {where: { id }})
+    }
+
+    changeDepartment(id: string, department_id: string) {
+        return this._dbClient.update({department_id: department_id}, {where: { id }})
+    }
+
+    changeAvatar(id: string, avatar_id: Date) {
+        return this._dbClient.update({avatar_id: avatar_id}, {where: { id }})
     }
 }
 
--- Ecampus.Services.Profile/src/services/profile.ts
+++ Ecampus.Services.Profile/src/services/profile.ts
@@ -18,16 +18,32 @@ class ProfileService {
         return profile;
     }
 
-    create(id: string) {
-        return id;
+    async create(login: string, password: string, first_name: string, last_name: string, birth_date: Date, role_id: number) {
+        await this.profileRepository.createNewUser({login, password, first_name, last_name, birth_date, role_id})
     }
 
-    delete(id: string) {
-        return id;
+    async delete(id: string) {
+        await this.profileRepository.deleteById(id);
     }
 
-    update(id: string) {
-        return id;
+    async changeName(id: string, first_name: string, last_name: string) {
+        await this.profileRepository.changeName(id, first_name, last_name)
+    }
+
+    async changeRole(id: string, role_id: string) {
+        await this.profileRepository.changeRole(id, role_id)
+    }
+
+    async changeGroup(id: string, group_id: string) {
+        await this.profileRepository.changeGroup(id, group_id)
+    }
+
+    async changeDepartment(id: string, department_id: string) {
+        await this.profileRepository.changeDepartment(id, department_id)
+    }
+
+    async changeAvatar(id: string, avatar_id: Date) {
+        await this.profileRepository.changeAvatar(id, avatar_id)
     }
 }
 
