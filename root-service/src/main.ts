f3a3eb8b462cc97493b8de5491c3deb318295690
Maksym <rendon4ik@gmail.com>
2022-11-20 17:04:33 +0200

    Added logout endpoint to root service

--- root-service/src/main.ts
+++ root-service/src/main.ts
@@ -7,13 +7,13 @@ const port = 8080
 const PORT = 80;
 const HOST = 'localhost';
 
-app.get('/api/root-service/file-service/test', async (req, res) => {
+app.get('/api/root-service/file-service/test', async (_req, res) => {
     const result = await axios.get(`http://file-service/api/file-service/file/4b568892-9338-4c69-9dcd-dde183878727`)
-        .then(function (response) {
+        .then(function (response: any) {
             // handle success
             console.log(response);
         })
-        .catch(function (error) {
+        .catch(function (error: any) {
             console.log('Error');
 
             console.log(error);
@@ -30,7 +30,30 @@ app.get('/api/root-service/file-service/test', async (req, res) => {
     return res.status(200).json(result);
 })
 
-app.get('/api/root-service/', (req, res) => {
+app.post('/api/root-service/auth/logout', async (_req, res) => {
+    const result = await axios.post(`http://auth-service/api/auth/logout`)
+        .then(function (response: any) {
+            // handle success
+            console.log(response);
+        })
+        .catch(function (error: any) {
+            console.log('Error');
+
+            console.log(error);
+            if (error?.response?.status === 400) {
+                return { message: 'Success!' }
+            }
+        })
+        .finally(function () {
+            // always executed
+        });
+    console.log(result);
+
+
+    return res.status(200).json(result);
+})
+
+app.get('/api/root-service/', (_req, res) => {
     res.send('Service works!')
 })
 
