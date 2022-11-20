1ac781f1cf5580aa2c6ee2600d4e5ed9fd23ea63
Maksym <rendon4ik@gmail.com>
2022-11-20 17:10:04 +0200

    Update route for profile service

--- ECampus.Services.Auth/Service/Sync/HttpSyncClient.cs
+++ ECampus.Services.Auth/Service/Sync/HttpSyncClient.cs
@@ -9,7 +9,7 @@ namespace ECampus.Services.Auth.Sync
         public async Task<User> GetUserByUsernameAsync(string username)
         {
             using HttpClient client = new();
-            var res = await client.GetAsync($"http://localhost:80/api/profile/username/{username}");
+            var res = await client.GetAsync($"http://profile-service/api/profile/username/{username}");
             if (!res.IsSuccessStatusCode) return null;
 
             var jsonString = await res.Content.ReadAsStringAsync();
@@ -26,7 +26,7 @@ namespace ECampus.Services.Auth.Sync
             });
             StringContent content = new(body, Encoding.UTF8, "application/json");
 
-            var res = await client.PostAsync("http://localhost:80/api/profile", content);
+            var res = await client.PostAsync("http://profile-service/api/profile", content);
             if (!res.IsSuccessStatusCode) return null;
 
             var jsonString = await res.Content.ReadAsStringAsync();
