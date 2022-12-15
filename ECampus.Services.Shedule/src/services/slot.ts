cc4a50bd122524f750ff34e6710340a168b0be2b
vlad <vladshpilka86@gmail.com>
2022-12-15 19:33:49 +0200

    add kafka

--- ECampus.Services.Shedule/src/services/slot.ts
+++ ECampus.Services.Shedule/src/services/slot.ts
@@ -1,7 +1,20 @@
 import { SlotNotFoundError } from 'exceptions';
 import { SlotRepository } from 'db/repositories/slot';
 import crypto from 'crypto';
+import { Kafka } from 'kafkajs';
 
+
+const kafka = new Kafka({
+    clientId: 'schedule-manager',
+    brokers: ['kafka:9092'],
+    connectionTimeout: 100000,
+    retry: {
+        initialRetryTime: 10000,
+        retries: 10
+    }
+})
+
+const producer = kafka.producer();
 class SlotService {
     private slotRepository: SlotRepository;
 
@@ -16,6 +29,15 @@ class SlotService {
             throw new SlotNotFoundError();
         }
 
+        producer.connect().then(() => {
+            producer.send({
+                topic: 'schedule',
+                messages: [
+                    { value: JSON.stringify({ event: 'GETED', date: new Date(), data: slot }) },
+                ],
+            })
+        });
+        
         return slot;
     }
 
@@ -27,11 +49,29 @@ class SlotService {
             begin_time: new Date("1970-01-01, " + begin_time),
             end_time: new Date("1970-01-01, " + end_time)
         });
+
+        producer.connect().then(() => {
+            producer.send({
+                topic: 'schedule',
+                messages: [
+                    { value: JSON.stringify({ event: 'CREATED', date: new Date(), data: slot }) },
+                ],
+            })
+        });
+
         return slot;
     }
 
     async delete(id: string) {
         const slot = await this.slotRepository.delete(id);
+        producer.connect().then(() => {
+            producer.send({
+                topic: 'schedule',
+                messages: [
+                    { value: JSON.stringify({ event: 'DELETED', date: new Date(), data: slot }) },
+                ],
+            })
+        });
         return slot;
     }
 
@@ -41,6 +81,14 @@ class SlotService {
             day,
             begin_time: new Date("1970-01-01, " + begin_time),
             end_time: new Date("1970-01-01, " + end_time)
+        });
+        producer.connect().then(() => {
+            producer.send({
+                topic: 'schedule',
+                messages: [
+                    { value: JSON.stringify({ event: 'UPDATED', date: new Date(), data: slot }) },
+                ],
+            })
         });
         return slot;
     }
